a3d029e3f5214348e5e5b9967e34683439b33bf9
Maksix <armageddon4839@gmail.com>
2020-10-15 22:11:59 +0300

    new one

--- src/client/index.js
+++ src/client/index.js
@@ -1,26 +1,10 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import createLogger from 'redux-logger'
-import thunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'                                                                                                                                                    
-import {storeStateMiddleWare} from './middleware/storeStateMiddleWare'
-import reducer from './reducers'
-import App from './containers/app'
-import {alert} from './actions/alert'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const initialState = {}
-
-const store = createStore(
-  reducer,
-  initialState,
-  applyMiddleware(thunk, createLogger())
-)
-
-ReactDom.render((
-  <Provider store={store}>
-    <App/>
-  </Provider>
-), document.getElementById('tetris'))
-
-store.dispatch(alert('Soon, will be here a fantastic Tetris ...'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('tetris')
+);
