06b45cbe4289b53aeb2cc09235d6d41a2e00ac90
Решетникова Валерия <vireshetnikova@domclick.ru>
2020-10-18 14:00:22 +0300

    add less loader

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import styles from './App.less'
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       here
     </div>
   );
