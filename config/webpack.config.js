64a7859c6a1d8654da2e18434154e6b75e969508
Решетникова Валерия <vireshetnikova@domclick.ru>
2020-10-18 14:48:35 +0300

    add example component with less

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const fs = require('fs');
 const path = require('path');
@@ -502,6 +502,9 @@ module.exports = function(webpackEnv) {
                   loader: "less-loader",
                   options: {
                     sourceMap: false,
+                    lessOptions: {
+                      paths: [path.resolve('src')]
+                    }
                   }
                 }
               ]
--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Button} from "./Button";
 import styles from './App.less'
 
 function App() {
   return (
     <div className={styles.App}>
-      here
+      <Button onClick={()=>console.log('here')} text="Начать игру"/>
+      <Button onClick={()=>console.log('here')} theme="light" text="Начать игру"/>
     </div>
   );
 }
