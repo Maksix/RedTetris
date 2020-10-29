cd5418ad37b0a5d80e05856f0439d5f1ce6bdbee
Maksix <armageddon4839@gmail.com>
2020-10-29 21:28:39 +0300

    first steps for switching color, changed outline to darken/lighten background for lang and color switcher

--- src/client/components/MainScreen/ColorSwitcher/ColorSwitcher.jsx
+++ src/client/components/MainScreen/ColorSwitcher/ColorSwitcher.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
 import cn from 'classnames';
 import styles from './ColorSwitcher.module.less';
 
-const ColorSwitcher = () => (
-  <div className={cn(styles.colorBox)}>
-    <span className={cn('material-icons', styles.icon)}>
-      wb_sunny
-    </span>
-  </div>
-);
+const ColorSwitcher = () => {
+  const [theme, setTheme] = useState('dark');
+  return (
+    <div
+      tabIndex={0}
+      role="button"
+      onKeyDown={(e) => {
+        if (e.keyCode === 13) setTheme(theme => theme === 'dark' ? 'light' : 'dark');
+      }}
+      onClick={() => setTheme(theme => theme === 'dark' ? 'light' : 'dark')}
+      className={cn(styles.colorBox, styles[theme])}
+    >
+      <span className={cn('material-icons', styles.icon)}>
+        {theme === 'dark' ? 'wb_sunny' : 'brightness_3'}
+      </span>
+    </div>
+  );
+};
 
 export default ColorSwitcher;
