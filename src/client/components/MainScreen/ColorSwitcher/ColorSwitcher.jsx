424be639b6d45cc9c52571389ce84a8fda22d3f0
Maksix <armageddon4839@gmail.com>
2020-10-31 23:09:33 +0300

    added custom hook to change body styles on color switch without rerender App

--- src/client/components/MainScreen/ColorSwitcher/ColorSwitcher.jsx
+++ src/client/components/MainScreen/ColorSwitcher/ColorSwitcher.jsx
@@ -5,23 +5,31 @@ import cn from 'classnames';
 import PropTypes from 'prop-types';
 import styles from './ColorSwitcher.module.less';
 import { switchTheme } from '../../../actions/themeAction';
+import useComponentDidUpdate from "../../../hooks/useComponentDidUpdate"
 
-const ColorSwitcher = ({ theme, switchTheme }) => (
-  <div
-    style={{ '--background': theme === 'dark' ? 'black' : '#FCFFF4' }}
-    tabIndex={0}
-    role="button"
-    onKeyDown={(e) => {
-      if (e.keyCode === 13) switchTheme(theme === 'dark' ? 'light' : 'dark');
-    }}
-    onClick={() => switchTheme(theme === 'dark' ? 'light' : 'dark')}
-    className={cn(styles.colorBox, styles[theme])}
-  >
-    <span className={cn('material-icons', styles.icon)}>
-      {theme === 'dark' ? 'wb_sunny' : 'brightness_3'}
-    </span>
-  </div>
-);
+const ColorSwitcher = ({ theme, switchTheme }) => {
+
+  useComponentDidUpdate(() => {
+    const body = document.getElementsByTagName('body')[0];
+    body.style = theme === 'dark' ? 'background-color: black; transition: 1s;' : 'background-color: #FCFFF4; transition: 1s;';
+  }, [theme])
+  return (
+    <div
+      style={{ '--background': theme === 'dark' ? 'black' : '#fcfff4' }}
+      tabIndex={0}
+      role="button"
+      onKeyDown={(e) => {
+        if (e.keyCode === 13) switchTheme(theme === 'dark' ? 'light' : 'dark');
+      }}
+      onClick={() => switchTheme(theme === 'dark' ? 'light' : 'dark')}
+      className={cn(styles.colorBox, styles[theme])}
+    >
+      <span className={cn('material-icons', styles.icon)}>
+        {theme === 'dark' ? 'wb_sunny' : 'brightness_3'}
+      </span>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   theme: state.theme.theme,
