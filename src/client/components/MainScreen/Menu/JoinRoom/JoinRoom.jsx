d8e4dcc088a3b309104bbdb7e3e96d481b5f66b7
Maksix <armageddon4839@gmail.com>
2020-11-02 22:43:43 +0300

    added check for length of room name, added button to join room

--- src/client/components/MainScreen/Menu/JoinRoom/JoinRoom.jsx
+++ src/client/components/MainScreen/Menu/JoinRoom/JoinRoom.jsx
@@ -8,20 +8,35 @@ import styles from './JoinRoom.module.less';
 const JoinRoom = ({ theme }) => {
   const { t } = useTranslation();
   const [roomName, setRoomName] = useState('');
+  const [showButton, setShowButton] = useState(false);
   const onFocusOut = (e) => (e.target.placeholder = t('main.joinRoom'));
   const onFocus = (e) => (e.target.placeholder = t('main.joinRoomPlaceholder'));
+  const onRoomNameChange = (e) => {
+    setShowButton(e.target.value.length >= 5);
+    console.log(e.target.value.length);
+    if (e.target.value.length <= 5) setRoomName(e.target.value);
+  };
 
   return (
-    <div className={cn(styles.inputBox, styles[theme])}>
-      <input
-        type="text"
-        className={cn(styles.input, styles[theme])}
-        onChange={(e) => setRoomName(e.target.value)}
-        value={roomName}
-        placeholder={t('main.joinRoom')}
-        onBlur={(e) => onFocusOut(e)}
-        onFocus={(e) => onFocus(e)}
-      />
+    <div className={cn(styles.joinBox)}>
+      <div className={cn(styles.inputBox, styles[theme])}>
+        <input
+          type="text"
+          className={cn(styles.input, styles[theme])}
+          onChange={(e) => onRoomNameChange(e)}
+          value={roomName}
+          placeholder={t('main.joinRoom')}
+          onBlur={(e) => onFocusOut(e)}
+          onFocus={(e) => onFocus(e)}
+        />
+      </div>
+      {showButton && (
+        <div className={cn(styles.joinButton, styles[theme])}>
+          <span className={cn('material-icons', styles.joinIcon)}>
+            play_arrow
+          </span>
+        </div>
+      )}
     </div>
   );
 };
