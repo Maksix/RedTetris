7ca454e72c397fea9c58146d0cfc897f310a0781
Maksix <armageddon4839@gmail.com>
2020-11-06 21:44:34 +0300

    added modal for username

--- src/client/reducers/rootReducer.js
+++ src/client/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import themeReducer from './themeReducer';
+import usernameReducer from './usernameReducer';
 
 const rootReducer = combineReducers({
   theme: themeReducer,
+  username: usernameReducer,
 });
 
 export default rootReducer;
