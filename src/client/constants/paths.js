0bcd0b32c86094b441ebc5496debf656a6525ef1
Maksix <armageddon4839@gmail.com>
2020-11-08 22:45:48 +0300

    removed some packages

--- src/client/constants/paths.js
+++ src/client/constants/paths.js
@@ -1,18 +1,4 @@
-/* eslint-disable */
-
-import { pathToRegexp } from 'path-to-regexp';
-
-const keys = [
-  {
-    name: 'room', optional: false, repeat: false, pattern: '[A-Z0-9]{5}',
-  },
-  {
-    name: 'name', optional: false, repeat: false, pattern: '[A-Za-z0-9]{4,12}',
-  },
-];
-const gameRegExp = /\/#[A-Z0-9]{5}\[[A-Za-z0-9]{4,12}$/;
-const regexpPath = pathToRegexp('/#:room[:name]', keys)
 export const paths = {
   mainScreen: '/',
-  game: "/:room([A-Z0-9]{5})[:name([A-Za-z0-9]{4,12})]",
+  game: '/:room([A-Z0-9]{5})[:name([A-Za-z0-9]{4,12})]',
 };
