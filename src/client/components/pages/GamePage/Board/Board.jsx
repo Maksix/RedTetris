69481a6af50633fcdf40a2d5e33c668e2f24712e
Решетникова Валерия <vireshetnikova@domclick.ru>
2020-11-09 10:21:22 +0300

    add rule for custom hook, add mocked board

---
+++ src/client/components/pages/GamePage/Board/Board.jsx
@@ -0,0 +1,39 @@
+import React, { useMemo } from 'react';
+import { getFilledArray } from 'helpers/getFilledArray';
+import styles from './Board.less';
+
+const rows = getFilledArray(20);
+const row = getFilledArray(10).map(() => ({
+  backgroundColor: 'grey',
+}));
+const board = rows.fill(row);
+
+const test = [{ backgroundColor: 'red' },
+  { backgroundColor: 'green' },
+  { backgroundColor: 'blue' },
+  { backgroundColor: 'red' },
+  { backgroundColor: 'green' },
+  { backgroundColor: 'blue' },
+  { backgroundColor: 'red' },
+  { backgroundColor: 'red' },
+  { backgroundColor: 'green' },
+  { backgroundColor: 'blue' }];
+board[19] = test;
+
+export const Board = () => {
+  const boardContent = useMemo(() => board.map((rowItem, rowInd) => {
+    const rowContent = rowItem.map(({ backgroundColor }, cellInd) => (
+      <div className={styles.cell} style={{ backgroundColor }} key={cellInd} />
+    ));
+
+    return (
+      <div key={rowInd}>
+        <div className={styles.row}>{rowContent}</div>
+      </div>
+    );
+  }), []);
+
+  return (
+    <div>{boardContent}</div>
+  );
+};
