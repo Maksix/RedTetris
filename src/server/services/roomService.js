2da2d09dffea9cff0b37fd7214b76fc27fb6b7e4
Суржиков Максим <Surzhikov.M.L@sberbank.ru>
2020-11-29 21:58:11 +0300

    now deletes room when last player leaves

--- src/server/services/roomService.js
+++ src/server/services/roomService.js
@@ -1,5 +1,6 @@
 const Room = require('../classes/Room/Room');
 const Player = require('../classes/Player/Player');
+
 const {
   OUT_JOIN_ROOM, JOIN_ROOM_ERROR, UPDATE_PLAYER_LIST, OUT_LEAVE_ROOM, UPDATE_ROLE,
 } = require('./types');
@@ -7,9 +8,11 @@ const {
 const onJoinRoom = (socket, io, rooms) => {
   socket.on(OUT_JOIN_ROOM, ({ playerName, roomName }) => {
     let currentRoom = rooms.find((room) => room.name === roomName);
+    console.log(currentRoom);
     if (!currentRoom) {
       currentRoom = new Room(roomName, new Player(playerName, socket.id, 'leader'));
       rooms.push(currentRoom);
+      console.log('here');
       socket.emit(UPDATE_ROLE, 'leader');
     } else if (currentRoom.canJoin()) {
       currentRoom.addPlayer(new Player(playerName, socket.id));
@@ -30,11 +33,13 @@ const onLeaveRoom = (socket, io, rooms) => {
       currentRoom.removePlayer(socket.id);
       const newLeader = currentRoom?.players?.find((player) => player.role === 'leader');
       socket.leave(roomName);
+      socket.emit(UPDATE_ROLE, 'player');
       if (newLeader?.id) {
         io.to(newLeader.id).emit(UPDATE_ROLE, 'leader');
+        io.in(roomName).emit(UPDATE_PLAYER_LIST, currentRoom?.players);
+      } else {
+        rooms.splice(rooms.findIndex((room) => room.name === roomName), 1);
       }
-      socket.emit(UPDATE_ROLE, 'player');
-      io.in(roomName).emit(UPDATE_PLAYER_LIST, currentRoom?.players);
     }
   });
 };
